a68e686815d06380b375fa9bdcbd0f5b8997a7eb
piragenthuni <piragenthuni@gmail.com>
2025-04-09 08:00:50 +0530

    lab ca

--- Fundamentals of Programming/Labsheet02/labsheet02-05.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(){
-    float length, width , area;
-    length = 25.12428;
-    width = 12.59;
-    area = length * width;
-    printf("%.4f",area);
-    return 0;
-}
--- Fundamentals of Programming/Labsheet02/labsheet02-3book.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main (){
-    struct Book{
-    char title[100];
-    char author[50];
-    double price;
-    };str
-return 0}
